2e0c6e0ef29bcaf26528fc3ad44ab87f4f0e7dca
Anabelly <anabellybel123@gmail.com>
2024-07-16 11:19:09 -0300

    first commit

---
+++ lib/screens/categories_sreens.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+
+class CategoriesSreens extends StatelessWidget {
+  const CategoriesSreens({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+      maxCrossAxisExtent: 200,
+      childAspectRatio:  3 / 2,
+      crossAxisSpacing: 20,
+      mainAxisSpacing:  20,
+      ) ,
+      children: const <Widget>[
+        
+      ],
+    );
+  }
+}

ada7145d61eb17bd69c4ce6e88bd3543e76c7cba
Anabelly <anabellybel123@gmail.com>
2024-07-24 20:42:38 -0300

    rota

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'package:flutter/material.dart';
 import './screens/categories_sreens.dart';
-
-
+import './screens/categories_meals_screen.dart';
 void main() => runApp(const MyApp());
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-        hintColor: Colors.amber,
-        fontFamily:'Raleway',
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
-        textTheme: ThemeData.light().textTheme.copyWith(
-          titleLarge: const TextStyle(
-             fontSize: 20,
-             fontFamily: 'RobotoCondensed'
-          ),
+        title: 'DeliMeals',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          hintColor: Colors.amber,
+          fontFamily: 'Raleway',
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          textTheme: ThemeData.light().textTheme.copyWith(
+                titleLarge: const TextStyle(
+                    fontSize: 20, fontFamily: 'RobotoCondensed'),
+              ),
         ),
-        
-      ),
-      home: const CategoriesSreens(),
-    );
+        home: const CategoriesSreens(),
+        routes: {
+   '/categories-meals': (ctx) => const CategoriesMealsScreen(),
+          },
+          );
+          
   }
 }
- 
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+
     return Scaffold(
-  appBar: AppBar(
-    title:const Text( 'Receitas'),
-  ),
-    body: const Center (child: Text (' Receitas por Categorias'),
-    ),
-    ); 
+      appBar: AppBar(
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text('Receitas por Categoria ${category.id}'),
+      ),
+    );
   }
 }

3a19261edf981eba6f1c5157933c33d583982e51
Anabelly <anabellybel123@gmail.com>
2024-07-26 10:52:52 -0300

    seleção por imagem

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:app_comidas/screens/categories_sreens.dart';
 import 'package:flutter/material.dart';
-import 'screens/categories_screen.dart';
 import 'screens/categories_meals_screen.dart';
 import 'utils/app_routes.dart';
 
